8e69c33ed182f87fab8fac22837dfb9efcb20e8d
firmansyahm <firmansyah.maulana.ti@gmail.com>
2019-11-20 17:44:32 +0700

    Update array_urang.php

--- array_urang.php
+++ array_urang.php
@@ -1,3 +1,67 @@
+<?php
+
+function my_custom_menu_page(){ 
+
+    $array = data_products();
+    echo "<pre>";
+    var_dump( $array );
+    echo "</pre>";
+   
+    ?>
+    <div>
+      <form method="POST">
+        <input type="submit" name="submit" value="submit">
+      </form>
+
+      <form method="POST" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+          <input type="hidden" name="action" value="generate_csv" />
+          <input type="submit" value="Do it!" />
+      </form>
+    </div>
+  <? }
+
+  add_action( 'admin_post_generate_csv', 'generate_csv_admin_action' );
+
+  function generate_csv_admin_action()
+  {
+      download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+      $array = data_products();
+      echo array2csv($array);
+      die();
+  }
+
+  function download_send_headers($filename) {
+        // disable caching
+      $now = gmdate("D, d M Y H:i:s");
+      header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+      header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
+      header("Last-Modified: {$now} GMT");
+
+      // force download  
+      header("Content-Type: application/force-download");
+      header("Content-Type: application/octet-stream");
+      header("Content-Type: application/download");
+
+      // disposition / encoding on response body
+      header("Content-Disposition: attachment;filename={$filename}");
+      header("Content-Transfer-Encoding: binary");
+  }
+
+  function array2csv(array &$array)
+  {
+     if (count($array) == 0) {
+       return null;
+     }
+     ob_start();
+     $df = fopen("php://output", 'w');
+     fputcsv($df, array_keys(reset($array)));
+     foreach ($array as $row) {
+        fputcsv($df, $row);
+     }
+     fclose($df);
+     return ob_get_clean();
+  }
+
 function data_products() {
 
     $data       = array();
